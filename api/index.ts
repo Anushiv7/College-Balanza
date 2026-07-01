e086a9328aeab68a76d8012a7e7c5090e8aef7fd
Anushiv7 <prakashanushiv@gmail.com>
2026-07-01 23:43:07 +0530

    trying to fix backedn 202

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,5 @@
-import "../server/types/express";
 import { createApp } from "../server/_core/index";
 
-// Vercel's `@vercel/node` runtime accepts an Express app as the default export.
 const app = createApp();
 
 export default app;
